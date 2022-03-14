4914fb8e235f3e1af0ea7627adc8804f937bc3ee
minjee <mjwoo0@naver.com>
2022-03-14 21:15:50 +0900

    pg.138

--- Python_algorithm_interview/138.py
+++ Python_algorithm_interview/138.py
@@ -1,4 +1,20 @@
 # 파이썬 알고리즘 인터뷰 
+from collections import deque
+
+# deque 자료구조형을 이용한 풀이  
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        q = deque()
+        
+        for char in s:
+            if char.isalnum():
+                q.append(char.lower())
+    
+        while len(q)>1:
+            if q.popleft() != q.pop():
+                return False
+        return True 
+    
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
@@ -11,4 +27,3 @@ class Solution:
             if string.pop(0) != string.pop():
                 return False
         return True 
-        
