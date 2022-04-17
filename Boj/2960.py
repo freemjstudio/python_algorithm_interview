2cbf857c9382609913252bdcf734bc0e4f790873
minjee <mjwoo0@naver.com>
2022-04-17 09:02:33 -0700

    에라토스테네스의 체

--- Boj/2960.py
+++ Boj/2960.py
@@ -1,23 +1,17 @@
 # 2960 에라토스테네스의 체 
 
-from re import T
-
-
 n, k = map(int,input().split())
-count = 1
-
+count = 0
 numbers = [True for _ in range(n+1)]
-numbers[2] = False 
-p = 2 # 첫번째 p , 가장 작은 소수: 2
-now = p
-while True:
-    if count == k:
-        break
-    for i in range(p+1, n+1):
-        if i%p == 0 and numbers[i] == True:
-            numbers[i] = False
-            count += 1
-            now = i
+for i in range(2, n+1):
+    if numbers[i] == True:
+        for j in range(i, n+1, i):
+            if numbers[j] == True: # 아직 지우지 않은 수이면
+                numbers[j] = False # 지운다 
+                count += 1 # 몇번째 지워진 수 인지 확인한다
+            if count == k: # k 번째 지워진 수라면 답 출력 
+                print(j)
+                break 
+            
 
-print(now)
     
