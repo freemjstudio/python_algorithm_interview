5797d7375b8a70a9a481cb4b982de2fb4dd4f1c3
minjee <mjwoo0@naver.com>
2022-04-16 23:02:38 -0700

    11653 소인수분해

--- Boj/11653.py
+++ Boj/11653.py
@@ -1,3 +1,13 @@
 # 소인수 분해 
 n = int(input())
 
+div = 2
+while True:
+    if n == 1:
+        break 
+    if n % div == 0:
+        n = n/div
+        print(div)
+    else:
+        div += 1
+        
