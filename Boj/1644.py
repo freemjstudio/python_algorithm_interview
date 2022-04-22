dc1ddbc9589ebc9ce0a1ce39ec08b6d87d7867ac
minjee <mjwoo0@naver.com>
2022-04-22 08:49:13 -0700

    1644소수의부분합

--- Boj/1644.py
+++ Boj/1644.py
@@ -1,23 +1,27 @@
 # 소수의 연속합
-result = 0 
+count = 0 
 n = int(input())
 check = [True]*(n+1) 
 numbers = [] # 소수 인 숫자들의 배열 
 
 for i in range(2, n+1):
     if check[i] == True:
+        numbers.append(i)
         for j in range(i, n+1, i):
             if check[j] == True:
                 check[j] = False
 
-for i in range(2, n+1):
-    if check[i] == True:
-        numbers.append(i) # 지워지지 않은 숫자는 소수 
-
 # two pointer 알고리즘                 
-start = 0
 end = 0 
-
+interval_sum = 0 
+for start in range(len(numbers)):
+    while interval_sum < n and end < len(numbers):
+        interval_sum += numbers[end]
+        end += 1
+    if interval_sum == n:
+        count += 1
+    interval_sum -= numbers[start]
+    
 # false 이면 소수
-print(result)
+print(count)
 
--- Boj/two_pointer.py
+++ Boj/two_pointer.py
@@ -1,6 +1,7 @@
 n = 5 # 데이터의 개수 
 m = 5 # 찾고자 하는 부분합 M
 data = [i for i in range(1, n+1)]
+
 count = 0 
 interval_sum = 0 
 end = 0 
@@ -9,3 +10,9 @@ for start in range(n):
     # end 만큼 이동시키기 
     while interval_sum < m and end <n:
         interval_sum += data[end]
+        end += 1
+    if interval_sum == m:
+        count += 1
+    interval_sum -= data[start] # start 포인터를 이동시키므로 이전 값은 빼주어야 한다. 
+
+print(count)
