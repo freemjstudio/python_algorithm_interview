a55d5c53c68e484131e43eaffb5d8485a549ef41
minjee <mjwoo0@naver.com>
2022-04-26 07:52:09 -0700

    10942

--- Boj/10942.py
+++ Boj/10942.py
@@ -1,6 +1,26 @@
 # 팰린드롬?
+import deque
+
 n = int(input())
-data = list(map(int, input()))
+data = [0]+list(map(int, input().split()))
 m = int(input())
+
+def check(s, e): # 펠린드롬 확인 
+    queue = deque(data[s:e+1])
+    length = len(array)
+    while len(queue) == 1:
+        if queue.popleft() != queue.pop():
+            return False
+    return True 
+
 for _ in range(m):
     s, e = map(int, input().split())
+    if check(s, e):
+        print(1)
+    else:
+        print(0)
+    
+
+    
+    
+    
