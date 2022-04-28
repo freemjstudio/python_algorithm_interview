971f6e343705ec3a9873647c5bf808982bc36e18
minjee <mjwoo0@naver.com>
2022-04-28 07:49:37 -0700

    9466

---
+++ Boj/9466.py
@@ -0,0 +1,18 @@
+# 9466 텀 프로젝트 
+
+t = int(input())
+    
+
+for _ in range(t):
+    n = int(input())
+    visited = [False]*(n+1)
+    data = [0] + list(map(int, input().split())) 
+    team = []
+    
+    
+    
+    #result 
+    print(n - len(team))
+    
+
+    
