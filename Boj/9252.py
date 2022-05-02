089fa5cbdea8e9b1806f1342f8b43fccb7f8f028
minjee <mjwoo0@naver.com>
2022-05-01 21:13:59 -0700

    LCS2

--- Boj/9252.py
+++ Boj/9252.py
@@ -1,8 +1,29 @@
 # 9252
 
-data1 = input()
-data2 = input()
+S1 = input()
+S2 = input()
 
-dp = []
+n = len(S1)
+m = len(S2)
 
+dp = [[0] * (m+1) for _ in range(n+1)] # length of the LCS
+dp2 = [[]*(m+1) for _ in range(n+1)] # alphabet dp 
+
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if S1[i-1] == S2[j-1]: # 같은 문자일 경우 
+            dp[i][j] = dp[i-1][j-1] + 1
+            dp2[i][j].append(dp[i-1][j-1])
+            dp2[i][j].append(S1[i-1])
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            if dp[i-1][j] > dp[i][j-1]:
+                dp2[i][j].append(dp2[i-1][j])
+            else:
+                dp2[i][j].append(dp2[i][j-1])
+            
+        
+print(dp[-1][-1])
+print(''.join(dp2[-1][-1]))
 
