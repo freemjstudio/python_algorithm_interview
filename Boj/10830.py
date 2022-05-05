10e1baa600f9affbb98c275f5c98d23e994150c9
minjee <mjwoo0@naver.com>
2022-05-04 17:52:04 -0700

    10830

--- Boj/10830.py
+++ Boj/10830.py
@@ -1,24 +1,10 @@
 # 10830 행렬 제곱 
 
-N, b = map(int, input().split())
-matrix = [list(map(int, input().split()) for _ in range(N))]
+N, B = map(int, input().split())
+array = []
 
-# 행렬 곱셈 함수 
-def multiply(matrix1,matrix2):
-    n = len(matrix1)
-    result = [[0]*n for _ in range(n)] 
-    for row in range(n):
-        for col in range(n):
-            e = 0
-            for i in range(n):
-                e += matrix1[row][i] * matrix2[i][col]
-            result[row][col] = e%1000
-    return result
-
-def square(B, M):
-    if B == 1:
-        for i in range(N):
-            for j in range(N):
-                M[i][j] %= 1000
-        return M 
+for _ in range(N):
+    array.append(list(map(int, input().split())))
+    
+    
     
--- Boj/2740.py
+++ Boj/2740.py
@@ -7,13 +7,17 @@ A = [list(map(int, input().split())) for _ in range(n)]
 m, k = map(int, input().split())
 B = [list(map(int, input().split())) for _ in range(m)]
 result = [[0]*k for _ in range(n)]
+# 행 is row 열 is column 
+
+# N*M 행렬 곱하기 M*K 행렬 = N*K 행렬 
 
 for row in range(n):
     for col in range(k):
-        e = 0 
+        element = 0 
         for i in range(m):
-            e += A[row][i] * B[i][col]
-        result[row][col] =  e
-    
+            element += A[row][i] * B[i][col]
+        result[row][col] = element
+
+
 for i in result:
     print(*i)
