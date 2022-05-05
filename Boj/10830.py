e811f66d30529d0108ca3d75c2559a59153b977b
minjee <mjwoo0@naver.com>
2022-05-04 18:24:31 -0700

    10830

--- Boj/10830.py
+++ Boj/10830.py
@@ -2,9 +2,19 @@
 
 N, B = map(int, input().split())
 array = []
-
+result = []
 for _ in range(N):
     array.append(list(map(int, input().split())))
-    
-    
-    
+
+# 홀수 , 짝수 처리 
+
+ 
+def square():
+    for row in range(N):
+        for col in range(N):
+            element = 0 
+            for i in range(N):
+                element += array[row][i] * array[i][col]
+            
+for i in result:
+    print(*i)
