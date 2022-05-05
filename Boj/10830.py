4dda90732f92b8cb7f187e5bb5f9cf42bf517c85
minjee <mjwoo0@naver.com>
2022-05-04 18:45:47 -0700

    행렬제곱

--- Boj/10830.py
+++ Boj/10830.py
@@ -1,5 +1,8 @@
 # 10830 행렬 제곱 
 
+from re import A
+
+
 N, B = map(int, input().split())
 array = []
 result = []
@@ -8,13 +11,31 @@ for _ in range(N):
 
 # 홀수 , 짝수 처리 
 
+def square(array, B): # 제곱처리하기 
+    if B == 1:
+        for i in range(N):
+            for j in range(N):
+                array[i][j] %= 1000
+        return array
+    
+    s_array = square(array, B//2)
+    if B % 2 == 0: # 짝수 (B//2)*(B//2)
+        return multiply(s_array, s_array)
+    else: # 홀수 (B//2)*(B//2)*1
+        return multiply(multiply(s_array, s_array), array)
  
-def square():
+def multiply(C, D): # 행렬 곱셈 연산 
+    temp = [[0]*N for _ in range(N)] # 크기는 어처피 N*N 행렬
+    
     for row in range(N):
         for col in range(N):
             element = 0 
             for i in range(N):
-                element += array[row][i] * array[i][col]
-            
+                element += C[row][i] * D[i][col]
+            temp[row][col] = element%1000
+    return temp
+
+result = square(array, B)   
+
 for i in result:
     print(*i)
