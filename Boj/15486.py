6fe968e138c14cda8c36042c86962629886fc679
minjee <mjwoo0@naver.com>
2022-07-13 17:30:24 -0700

    15486 DP

--- Boj/15486.py
+++ Boj/15486.py
@@ -4,9 +4,17 @@ n = int(input())
 
 t = [] # task 
 p = [] # price 
+dp = [0] * (n+1) # 해당일까지의 최대 이익을 저장 
 
 for _ in range(n):
     a, b = map(int, input().split())
+    t.append(a)
+    p.append(b)
+
+result = 0 
+
+for i in range(n): # 0 ~ n-1
+    result = max()
     
 
-    
+print(max(dp))
