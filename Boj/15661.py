88a13348ff5d7541564678a8e869480935e139fc
minjee <mjwoo0@naver.com>
2022-07-16 10:22:09 -0700

    15661

---
+++ Boj/15661.py
@@ -0,0 +1,17 @@
+# 링크와 스타트 
+from itertools import combinations
+
+n = int(input())
+data = []
+for _ in range(n):
+    data.append(map(int, input().split()))
+
+INF = int(1e9)
+result = INF # 스타트와 링크 능력치 차이 
+
+for i in range(1, int(n//2)+1): # 1~ n//2 까지 확인하기 
+    
+    
+    
+    
+    
