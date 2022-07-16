120ac126e8bfe06b42cb5b8a0ec5bedf5878294b
minjee <mjwoo0@naver.com>
2022-07-16 10:49:48 -0700

    15661 py

--- Boj/15661.py
+++ Boj/15661.py
@@ -4,17 +4,18 @@ from itertools import combinations
 n = int(input())
 data = []
 for _ in range(n):
-    data.append(map(int, input().split()))
+    data.append(list(map(int, input().split())))
 
 INF = int(1e9)
 result = INF # 스타트와 링크 능력치 차이 
 member = [i for i in range(n)]
 
-for i in range(1, int(n//2)+1): # 1~ n//2 까지 확인하기 
+for i in range(1, int(n/2)+1): # 1~ n//2 까지 확인하기 
     comb_team = combinations(member, i)
+    min_diff = INF
     for comb in comb_team: # 조합을 하나씩 조회하기 
         start = list(comb) # comb 는 set를 반환하므로 list로 바꿔주어야 함 
-        link = list(set(member)-comb)
+        link = list(set(member)-set(start))
         start_sum = 0 
         link_sum = 0 
         for x in range(n-1):
@@ -30,8 +31,8 @@ for i in range(1, int(n//2)+1): # 1~ n//2 까지 확인하기
                 start_sum += s
                 link_sum += l
     
-    
         diff = abs(start_sum - link_sum)
-    result = min(diff, result)
+        min_diff = min(diff, min_diff)
+    result = min(min_diff, result)
 
 print(result)
