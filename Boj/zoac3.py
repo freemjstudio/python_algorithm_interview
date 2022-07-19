240fba37f91ecb864f3e1bb0f27fa7d23d1be52c
minjee <mjwoo0@naver.com>
2022-07-19 08:27:40 -0700

    20436

--- Boj/zoac3.py
+++ Boj/zoac3.py
@@ -1,1 +1,8 @@
 # https://www.acmicpc.net/problem/20436 zoac3
+
+left, right = input().split()
+word = input()
+# 택시거리를 구하기 
+keyboard = ['qwertyuiop',
+            'asdfghjkl',
+            'zxcvbnm']
