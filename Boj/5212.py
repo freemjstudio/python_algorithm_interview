068891498d81e34e656cb5176c168d2c99565664
minjee <mjwoo0@naver.com>
2022-07-19 01:26:27 -0700

    20436

--- Boj/5212.py
+++ Boj/5212.py
@@ -16,23 +16,19 @@ for x in range(r):
         if data[x][y] == '.':
             continue
         cnt = 0 
+        
         for k in range(4):
             nx = dx[k] + x
             ny = dy[k] + y
     
-            if nx < 0 or nx >= r or ny < 0 or ny > c:
+            if nx < 0 or nx >= r or ny < 0 or ny >= c:
                 cnt += 1
                 continue
             elif data[nx][ny] == '.':
                 cnt += 1
         if cnt >= 3:
             result[x][y] = '.'
-
-print("")
-for i in range(r):
-    for j in range(c):
-        print(result[i][j],end='')
-    print()           
+         
 # 지도 영역 체크하기 
 start_row = 0 
 end_row = 0 
@@ -58,13 +54,11 @@ for i in range(start_row, end_row+1):
     # enumerate 로 X가 있는 index만 꺼냄 
     if not temp: # temp 가 비었을 때 
         continue
-    min_temp = temp[0] # 0번째 index 
-    max_temp = temp[-1] # 맨마지막 index 
+    min_temp = temp[0] # 0번째 index -> 2, 0 
+    max_temp = temp[-1] # 맨마지막 index -> 
     start_col = min(start_col, min_temp)
     end_col = max(end_col, max_temp)
 
-print(start_col)
-print(end_col)
 for x in range(start_row, end_row+1):
     for y in range(start_col, end_col+1):
         print(result[x][y], end="")
