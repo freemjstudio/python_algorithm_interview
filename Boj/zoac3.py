019ae9622e6973fa661f2bb926e8238468e88d0c
minjee <mjwoo0@naver.com>
2022-07-20 08:00:10 -0700

    20436

--- Boj/zoac3.py
+++ Boj/zoac3.py
@@ -1,4 +1,5 @@
 # https://www.acmicpc.net/problem/20436 zoac3
+import itertools
 
 left, right = input().split()
 word = input()
@@ -6,3 +7,13 @@ word = input()
 keyboard = ['qwertyuiop',
             'asdfghjkl',
             'zxcvbnm']
+
+x1, y1 = -1, -1 # a 좌표 
+x2, y2 = -1, -1 # b 좌표 
+
+# keyboard row 에 있는지 검사 -> column 에서 index() 함수 써서 위치 반환하기 
+for i in range(len(word)):
+    
+    
+    
+diff = abs(x1-x2) + abs(y1-y2) # 걸리는 시간 
