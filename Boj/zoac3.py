5e068fa8200840379666c8011fa163c9b0820031
minjee <mjwoo0@naver.com>
2022-07-21 08:22:08 -0700

    zoac3

--- Boj/zoac3.py
+++ Boj/zoac3.py
@@ -8,12 +8,30 @@ keyboard = ['qwertyuiop',
             'asdfghjkl',
             'zxcvbnm']
 
-x1, y1 = -1, -1 # a 좌표 
-x2, y2 = -1, -1 # b 좌표 
-
 # keyboard row 에 있는지 검사 -> column 에서 index() 함수 써서 위치 반환하기 
+def pos_check(alphabet):
+    a_x, a_y = -1, -1 
+    for i in range(3):
+        if alphabet in keyboard[i]:
+            a_x = i 
+            for j in range(len(keyboard[i])):
+                if keyboard[j] == alphabet:
+                    a_y = j 
+    
+    return a_x, a_y 
+
+left_x, left_y = pos_check(left)
+right_x, right_y = pos_check(right)
+result = 0 
+
 for i in range(len(word)):
+    word_x, word_y = pos_check(word[i])
+    if (abs(left_x-word_x)+ abs(left_y-word_y)) < (abs(right_x - word_x)+ abs(right_y - word_y)):
+        left_x, left_y = word_x, word_y
+        result += abs(left_x-word_x)+ abs(left_y-word_y)
+    else:
+        right_x, right_y = word_x, word_y
+        result += (abs(right_x - word_x)+ abs(right_y - word_y))
+
+print(result)
     
-    
-    
-diff = abs(x1-x2) + abs(y1-y2) # 걸리는 시간 
