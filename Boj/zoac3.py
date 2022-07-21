c54254c4906e77427b4ed82851f5e6e45387c47d
minjee <mjwoo0@naver.com>
2022-07-21 08:35:42 -0700

    20436

--- Boj/zoac3.py
+++ Boj/zoac3.py
@@ -1,5 +1,4 @@
 # https://www.acmicpc.net/problem/20436 zoac3
-import itertools
 
 left, right = input().split()
 word = input()
@@ -8,14 +7,18 @@ keyboard = ['qwertyuiop',
             'asdfghjkl',
             'zxcvbnm']
 
+# 자음쪽 -> 왼손 모음쪽 -> 오른손
+mo = 'yuiophjklbnm'
+
 # keyboard row 에 있는지 검사 -> column 에서 index() 함수 써서 위치 반환하기 
+
 def pos_check(alphabet):
     a_x, a_y = -1, -1 
-    for i in range(3):
+    for i in range(len(keyboard)):
         if alphabet in keyboard[i]:
             a_x = i 
             for j in range(len(keyboard[i])):
-                if keyboard[j] == alphabet:
+                if keyboard[i][j] == alphabet:
                     a_y = j 
     
     return a_x, a_y 
@@ -26,12 +29,13 @@ result = 0
 
 for i in range(len(word)):
     word_x, word_y = pos_check(word[i])
-    if (abs(left_x-word_x)+ abs(left_y-word_y)) < (abs(right_x - word_x)+ abs(right_y - word_y)):
-        left_x, left_y = word_x, word_y
-        result += abs(left_x-word_x)+ abs(left_y-word_y)
-    else:
-        right_x, right_y = word_x, word_y
-        result += (abs(right_x - word_x)+ abs(right_y - word_y))
-
-print(result)
+    result += 1 
+    if word[i] in mo: # right 
+        result += abs(right_x - word_x) + abs(right_y - word_y)
+        right_x, right_y = word_x, word_y 
+        
+    else: # left 
+        result += abs(left_x - word_x) + abs(left_y - word_y)
+        left_x, left_y = word_x, word_y 
     
+print(result)
