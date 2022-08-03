eb596ea16c2b6258094a26e4588dc69203a661a7
minjee <mjwoo0@naver.com>
2022-08-02 21:10:03 -0700

    2015

--- 2015.py
+++ 2015.py
@@ -6,5 +6,20 @@ array = list(map(int, input().split()))
 # A[1], A[2] ... A[N]
 
 # 합이 k 인 부분합의 개수 
+dp = [[0]*n for _ in range(n)]
 
-dp = []
+count = 0 
+
+for i in range(n):
+    for j in range(i, n):
+        if i == j:
+            dp[i][j] = array[i]
+        if i < j:
+            dp[i][j] = dp[i][j-1] + array[i]
+        
+for i in range(n):
+    for j in range(n):
+        if dp[i][j] == k:
+            count += 1
+            
+print(count)
