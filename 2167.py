118ba780e34c7b23ce8e0f43efe7af878a9f1774
minjee <mjwoo0@naver.com>
2022-08-03 10:13:05 -0700

    DP로 푸는 2167

--- 2167.py
+++ 2167.py
@@ -2,19 +2,19 @@
 
 N, M = map(int, input().split())
 data = []
+dp = [[0]*(M+1) for _ in range(N+1)]
+
 for _ in range(N):
     data.append(list(map(int, input().split())))
 
+# 누적합을 dp에 저장한다 
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + data[i-1][j-1]
+
 T = int(input())
+
 for _ in range(T):
-    a, b, x, y = map(int, input().split())
-    a -= 1
-    b -= 1
-    x -= 1
-    y -= 1
-    sum = 0 
-    for i in range(a, x+1):
-        for j in range(b, y+1):
-            sum += data[i][j]
-    print(sum)
+    i, j, x, y = map(int, input().split())
+    print(dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1])
     
