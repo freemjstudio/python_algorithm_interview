7c1f9c7e05ee90a831fe86ddfb15d03f72c8aab3
minjee <mjwoo0@naver.com>
2022-08-03 08:55:57 -0700

    2167

--- 2167.py
+++ 2167.py
@@ -1,2 +1,17 @@
 # 2차원 배열의 합 https://www.acmicpc.net/problem/2167
 
+N, M = map(int, input().split())
+data = []
+for _ in range(N):
+    data.append(list(map(int, input().split())))
+
+T = int(input())
+for _ in range(T):
+    a, b, x, y = map(int, input().split())
+    a -= 1
+    b -= 1
+    x -= 1
+    y -= 1
+    sum = 0 
+    for 
+    
