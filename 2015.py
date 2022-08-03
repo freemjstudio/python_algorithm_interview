51e0bd6251bf0a53b98d189c17ffc00ed865e9d8
minjee <mjwoo0@naver.com>
2022-08-02 21:40:15 -0700

    2015 누적합

--- 2015.py
+++ 2015.py
@@ -1,25 +1,21 @@
 # 2015 수들의 합 4
+from collections import defaultdict
+
+from traitlets import default # 딕셔너리이면서 디폴트 값을 정할 수 있음 
 
 n, k = map(int, input().split())
 array = list(map(int, input().split()))
 
-# A[1], A[2] ... A[N]
-
-# 합이 k 인 부분합의 개수 
-dp = [[0]*n for _ in range(n)]
-
+totalsum = defaultdict(int)
 count = 0 
 
+for i in range(1, n):
+    array[i] += array[i-1]
+
 for i in range(n):
-    for j in range(i, n):
-        if i == j:
-            dp[i][j] = array[i]
-        if i < j:
-            dp[i][j] = dp[i][j-1] + array[i]
-        
-for i in range(n):
-    for j in range(n):
-        if dp[i][j] == k:
-            count += 1
-            
+    if array[i] == k: # 누적합 체크 
+        count += 1
+    count += totalsum[array[i]-k] # S[i-1] == S[i] - k 이면 1 더해지고 아니면 0임 
+    totalsum[array[i]] += 1 # 누적합이 S[i] 이 되는 경우 (딕셔너리 값을 올려준다)
+    
 print(count)
