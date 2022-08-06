3e561ca7370398645ee23c708cdd4003af3c1ba8
minjee <mjwoo0@naver.com>
2022-08-06 10:11:27 -0700

    16439.py

--- 16439.py
+++ 16439.py
@@ -1,2 +1,15 @@
 # https://www.acmicpc.net/problem/16439 치킨치킨치킨 
+from itertools import combinations
 
+n, m = map(int, input().split())
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+result = 0 
+if n <= 3:
+    for i in range(n):
+        result += max(array[i])
+else:
+
+print(result)
+    
