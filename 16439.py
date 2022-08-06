5003139e24c8bba68eddee9fdfbe03501a56f88e
minjee <mjwoo0@naver.com>
2022-08-06 10:35:49 -0700

    16439

--- 16439.py
+++ 16439.py
@@ -3,6 +3,7 @@ from itertools import combinations
 
 n, m = map(int, input().split())
 array = []
+chicken = [i for i in range(0, m)]
 for i in range(n):
     array.append(list(map(int, input().split())))
 result = 0 
@@ -10,6 +11,17 @@ if n <= 3:
     for i in range(n):
         result += max(array[i])
 else:
-
+    comb = combinations(chicken, 3) # 3가지 종류만 주문할 수 있음 
+    for data in comb:
+        temp = []
+        temp_result = 0
+        for index in data:
+            for i in range(n):
+                temp.append(array[i][index])
+        temp.sort(reverse=True)
+        for i in range(n):
+            temp_result += temp[i]
+    result = max(result, temp_result)
+            
 print(result)
     
