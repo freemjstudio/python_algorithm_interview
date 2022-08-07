75899c50df667495a9748a474cde1927aa54c2d5
minjee <mjwoo0@naver.com>
2022-08-07 11:58:30 -0700

    11265.py

--- 11265.py
+++ 11265.py
@@ -1,2 +1,8 @@
 # https://www.acmicpc.net/problem/11265
 
+n, m = map(int, input().split()) # 파티장의 크기, 손님의 수 
+array = []
+for _ in range(n):
+    array.append(list(map(int, input().split())))
+for _ in range(m):
+    start, end, cost = map(int, input().split()) 
