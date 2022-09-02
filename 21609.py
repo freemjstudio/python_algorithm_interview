0ae2efb46f9f9693db22733983978b2ffbdafe78
minjee <mjwoo0@naver.com>
2022-09-01 18:20:13 -0700

    [leetcode] 217 python

--- 21609.py
+++ 21609.py
@@ -11,26 +11,12 @@ dy = [0, 0, -1, 1]
 for index in range(n*n):
     data = list(map(int, input().split()))
     student = data[0]
-    student_dict[student] = data[1:]
+    like_dict[student] = data[1:]
     
-    if index == 0:
-        mid = (n//2)
-        # 가운데에 배치하기 
-        seat_array[mid][mid] = student
-    else: # 첫번째 입력이 아닌 경우 
-        temp = [] # 자리 후보
-        # 좋아하는 학생 탐색 
-        for x in range(n):
-            for y in range(n):
-                if seat_array[x][y] in student_dict[student]:
-                    # temp.append((x,y))
-                    # (x,y) 상하좌우로 빈자리인지 확인하기 
-                    for i in range(4):
-                        nx = x + dx[i]
-                        ny = y + dy[i]
+    
                         
                     
-        
+
         
         # 조건 2번
         # 조건 3번 
---
+++ leetcode_217.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # 딕셔너리 [key:value]
+        check = {}
+        for i in nums:
+            if i in check.keys():
+                check[i] += 1
+            else:
+                check[i] = 1
+        
+        # key 값이 2인것이 있으면 return true else false
+        flag = false
+        for i in check.values():
+            if i >= 2:
+                flag = true 
+        return flag
+            
+        
